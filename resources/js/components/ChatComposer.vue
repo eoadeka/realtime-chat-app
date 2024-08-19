e063e901bc9e287adb253ebdecd12654a7998c88
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-08-19 22:36:52 +0100

    feat: add chat input form

---
+++ resources/js/components/ChatComposer.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+    <div class="chat-composer">
+        <input type="text" placeholder="Start typing your message..." v-model="messageText" @keyup.enter="sendMessage" />
+        <button class="btn btn-primary" @click="sendMessage">Send</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            // messageText: 'default'
+            messageText: ''
+        }
+    },
+    methods: {
+        sendMessage() {
+            // console.log('message sent!');
+            // console.log(this.messageText);
+            this.$emit('messagesent', {
+                message: this.messageText,
+                author: "John Doe"
+            });
+            this.messageText = '';
+        }
+    }
+}
+</script>
+
+<style lang="css">
+.chat-composer{
+    display: flex;
+}
+
+.chat-composer input{
+    flex: 1 auto;
+    color: black;
+}
+
+.chat-composer button{
+    border-radius: 0;
+}
+</style>
